18b0b51c6fe19fdbc91ad30ce8354fd404b937b9
John Dias <joaodias@google.com>
2018-06-22 12:27:38 -0700

    sched: [FIX] walt: fix out-of-bounds access
    A computation in update_top_tasks() is indexing
    off the end of a top_tasks array. There's code
    to limit the index in the computation, but it's
    insufficient.
    
    Bug: 110529282
    Change-Id: Idb5ff5e5800c014394bcb04638844bf1e057a40c
    Signed-off-by: John Dias <joaodias@google.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -714,6 +714,8 @@ static int load_to_index(u32 load)
 		return NUM_LOAD_INDICES - 1;
 	else
 		return load / sched_load_granule;
+	u32 index = load / sched_load_granule;
+	return min(index, (u32)(NUM_LOAD_INDICES - 1));
 }
 
 static void
