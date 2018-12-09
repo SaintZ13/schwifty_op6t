3121bc5ad67bc7f5cb4ab2eb314d6bd8c8f5a7c9
kdrag0n <dragon@khronodragon.com>
2018-12-08 22:05:08 -0800

    compiler-gcc: [FIX] optimize inlining
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -71,6 +71,11 @@
  * -Wunused-function.  This turns out to avoid the need for complex #ifdef
  * directives.  Suppress the warning in clang as well by using "unused"
  * function attribute, which is redundant but not harmful for gcc.
+ * Prefer gnu_inline, so that extern inline functions do not emit an
+ * externally visible function. This makes extern inline behave as per gnu89
+ * semantics rather than c99. This prevents multiple symbol definition errors
+ * of extern inline functions at link time.
+ * A lot of inline functions can cause havoc with function tracing.
  */
 #if !defined(CONFIG_ARCH_SUPPORTS_OPTIMIZED_INLINING) ||		\
     !defined(CONFIG_OPTIMIZE_INLINING) || (__GNUC__ < 4)
