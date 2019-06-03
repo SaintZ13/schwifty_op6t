5b4955cf8fcf78b9fddbba9be1c7a876b822a645
SaintZ13 <spaceghostpurp93@gmail.com>
2019-06-03 22:26:49 +0000

    schwifty: update gitignore

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Thu May 30 22:14:38 UTC 2019"
-#define LINUX_COMPILE_BY "saintz"
-#define LINUX_COMPILE_HOST "saintz"
-#define LINUX_COMPILER "gcc version 8.3.0 (GNU Toolchain for the A-profile Architecture 8.3-2019.03 (arm-rel-8.36)) "
